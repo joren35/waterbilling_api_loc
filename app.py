692506ee658e0ea5e01fd00fc888203ec2eec802
Joren Mundane <jorenpacaldo@yahoo.com>
2018-12-02 23:10:52 +0800

    Updated api

--- app.py
+++ app.py
@@ -44,18 +44,39 @@ def login():
 def register():
     params = request.get_json()
     username = params["username"]
-    firstname = params["firstname"]
-    lastname = params["lastname"]
     password = params["password"]
     number = params["number"]
-    address = params["address"]
     reg_key = params["reg_key"]
-    res = spcall('register', (username, firstname, lastname, password, number, False, address, reg_key), True)
+    res = spcall('register', (username, password, number, reg_key), True)
 
     if 'Error' in res[0][0]:
         return jsonify({'status': 'error', 'message': res[0][0]})
     elif 'Invalid' in res[0][0]:
-        return jsonify({'status': 'error', 'reason': 'Invalid Registration Key', 'message': res[0][0]})
+        return jsonify({'status': 'error', 'reason': 'Invalid Registration Key'})
+    elif 'used' in res[0][0]:
+        return jsonify({'status': 'error', 'reason': 'used'})
+    else:
+        return jsonify({'status': 'ok', 'user': res[0][0]})
+
+
+@app.route('/account/new', methods=['POST'])
+def add_account():
+    params = request.get_json()
+    firstname = params["firstname"]
+    lastname = params["lastname"]
+    address = params["address"]
+    act_key = params["act_key"]
+    reading = params["reading"]
+    amount = params["amount"]
+    rate = params["rate"]
+    cmused = params["cmused"]
+    date_issued = params["date_issued"]
+    due_date = params["due_date"]
+    s_status = params["s_status"]
+    res = spcall('add_account', (firstname, lastname, address, act_key, reading,amount,rate,cmused,date_issued,due_date,s_status), True)
+
+    if 'Error' in res[0][0]:
+        return jsonify({'status': 'error', 'message': res[0][0]})
     else:
         return jsonify({'status': 'ok', 'user': res[0][0]})
 
@@ -91,8 +112,8 @@ def dashboard(id):
     return jsonify({'status': 'ok', 'entries': recs, 'count': len(recs)})
 
 
-@app.route('/validate', methods=['POST'])
-def validator():
+@app.route('/validate/username', methods=['POST'])
+def validator_username():
     params = request.get_json()
     username = params["username"]
     res = spcall('username_validator', (username,), )
@@ -104,6 +125,18 @@ def validator():
     else:
         return jsonify({'status': res[0][0]})
 
+@app.route('/validate/key', methods=['POST'])
+def validator_key():
+    params = request.get_json()
+    actCode = params["actCode"]
+    res = spcall('key_validator', (actCode,), )
+
+    if 'Error' in res[0][0]:
+        return jsonify({'status': 'error', 'message': res[0][0]})
+    elif 'exist' in res[0][0]:
+        return jsonify({'status': 'exist', 'message': res[0][0]})
+    else:
+        return jsonify({'status': res[0][0]})
 
 @app.route('/bill/<string:id>', methods=['GET'])
 def get_bill_date(id):
@@ -118,14 +151,14 @@ def get_bill_date(id):
     return jsonify({'status': 'ok', 'entries': recs, 'count': len(recs)})
 
 
-@app.route('/users', methods=['GET'])
-def users():
-    res = spcall('get_names', (), )
+@app.route('/account/status/<string:status>', methods=['GET'])
+def get_acc_status(status):
+    res = spcall('activation_status', (status,), )
 
     recs = []
 
     for r in res:
-        recs.append({"id": r[0], "lastname": r[1], "firstname": r[2], "issued": r[3], "prev_reading":r[4]})
+        recs.append({"name": r[0], "code": r[1]})
     return jsonify({'status': 'ok', 'entries': recs, 'count': len(recs)})
 
 
